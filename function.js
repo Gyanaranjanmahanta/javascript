1720a9666e7a87556ab4e1451d867d6e73ad3268
Gyanaranjanmahanta <gyanaranjanmahanta35@gmail.com>
2024-09-11 13:01:37 +0000

    function

---
+++ function.js
@@ -0,0 +1,20 @@
+// function sum() {
+//  let a = 100;
+//  let b = 30;
+//  let c = a + b;
+//  console.log(c);
+// }
+
+// sum();
+// sum();
+// sum();
+
+
+//parametarise function
+
+function sum(x, y) {
+  let c = x + y;
+  console.log(c);
+}
+
+sum(2, 3);
